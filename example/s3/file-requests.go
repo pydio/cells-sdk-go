011e4127f87c33b5662db648754adef0716977c2
Bruno Sinou <bruno.sinou@posteo.de>
2018-06-08 11:03:51 +0200

    PoC: basic CRUD on file using the S3 SDK for go

--- example/s3/file-requests.go
+++ example/s3/file-requests.go
@@ -1,24 +1,46 @@
 package s3
 
 import (
-	"fmt"
+	"io"
 
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/pydio/cells-sdk-go/config"
 )
 
-func GetFile(workspaceSlug, pathToFile string, headOnly bool) error {
-
+func GetFile(workspaceSlug, pathToFile string) (*s3.GetObjectOutput, error) {
 	svc := s3.New(config.DefaultS3Session)
-
 	obj, err := svc.GetObject((&s3.GetObjectInput{}).
 		SetBucket(config.DefaultS3Config.Bucket).
 		SetKey(workspaceSlug + "/" + pathToFile),
 	)
 	if err != nil {
-		return err
+		return nil, err
 	}
+	return obj, nil
+}
 
-	fmt.Println("Got an object " + obj.String())
-	return nil
+func PutFile(workspaceSlug, pathToFile string, content io.ReadSeeker) (*s3.PutObjectOutput, error) {
+	svc := s3.New(config.DefaultS3Session)
+	obj, err := svc.PutObject((&s3.PutObjectInput{}).
+		SetBucket(config.DefaultS3Config.Bucket).
+		SetKey(workspaceSlug + "/" + pathToFile).
+		SetBody(content),
+	)
+	if err != nil {
+		return nil, err
+	}
+	return obj, nil
+}
+
+func DeleteFile(workspaceSlug, pathToFile string) (*s3.DeleteObjectOutput, error) {
+	svc := s3.New(config.DefaultS3Session)
+
+	obj, err := svc.DeleteObject((&s3.DeleteObjectInput{}).
+		SetBucket(config.DefaultS3Config.Bucket).
+		SetKey(workspaceSlug + "/" + pathToFile),
+	)
+	if err != nil {
+		return nil, err
+	}
+	return obj, nil
 }
--- example/s3/file-requests_test.go
+++ example/s3/file-requests_test.go
@@ -1,12 +1,15 @@
 package s3
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/pydio/cells-sdk-go/config"
 	. "github.com/smartystreets/goconvey/convey"
@@ -47,8 +50,63 @@ func TestMain(m *testing.M) {
 
 func TestS3Service(t *testing.T) {
 
-	Convey("Test simple Get", t, func() {
-		err := GetFile("", "", false)
+	Convey("CRUD via s3 works", t, func() {
+
+		wkspSlug := "basic"
+		testFileName := "test.txt"
+		testStr := "Simple test content"
+		testStr2 := testStr + " updated"
+
+		// Create
+		_, err := PutFile(wkspSlug, testFileName, strings.NewReader(testStr))
 		So(err, ShouldBeNil)
+		time.Sleep(1000 * time.Millisecond)
+
+		// Read
+		output, err := GetFile(wkspSlug, testFileName)
+		So(err, ShouldBeNil)
+		buf := new(bytes.Buffer)
+		i, err := buf.ReadFrom(output.Body)
+		retrievedStr := buf.String()
+		So(err, ShouldBeNil)
+		So(i, ShouldEqual, 19)
+		So(retrievedStr, ShouldEqual, testStr)
+
+		// Update
+		// fmt.Println("About to update with content: " + testStr2)
+		_, err = PutFile(wkspSlug, testFileName, strings.NewReader(testStr2))
+		So(err, ShouldBeNil)
+
+		// for j := 0; j < 10; j++ {
+		time.Sleep(1000 * time.Millisecond)
+		output, err = GetFile(wkspSlug, testFileName)
+		So(err, ShouldBeNil)
+		buf = new(bytes.Buffer)
+		i, err = buf.ReadFrom(output.Body)
+		retrievedStr2 := buf.String()
+		So(err, ShouldBeNil)
+		So(i, ShouldEqual, 27)
+
+		// fmt.Printf("retrieved length: %d \n", i)
+
+		// fmt.Printf("#%d: retrieved: [%s] \n", j, retrievedStr2)
+		So(retrievedStr2, ShouldEqual, testStr2)
+		// }
+
+		// Delete
+		_, err = DeleteFile(wkspSlug, testFileName)
+		So(err, ShouldBeNil)
+
+		// Try to retrieve deleted object
+		obj, err := GetFile(wkspSlug, testFileName)
+		So(err, ShouldNotBeNil)
+		So(obj, ShouldBeNil)
+
 	})
+
+	// Convey("Test simple Get", t, func() {
+	// 	err := GetFile("basic", "test/four.jpeg", false)
+	// 	So(err, ShouldBeNil)
+	// })
+
 }
