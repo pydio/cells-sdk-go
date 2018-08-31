37b7e7e6fdccd49fe0898cc8427d8ee7934b1efd
Bruno Sinou <bruno.sinou@posteo.de>
2018-08-31 10:47:29 +0200

    Merge commands from J.Thabet (#2)
    Add sample commands + some code cleaning.

---
+++ example/cmd/list_files.go
@@ -0,0 +1,60 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/spf13/cobra"
+
+	"github.com/pydio/cells-sdk-go/client/meta_service"
+	"github.com/pydio/cells-sdk-go/config"
+	"github.com/pydio/cells-sdk-go/models"
+)
+
+var listFiles = &cobra.Command{
+	Use:   "list-files",
+	Short: "lf",
+	Long:  `List files on pydio cells`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		//connects to the pydio api via the sdkConfig
+		httpClient := config.GetHttpClient(config.DefaultConfig)
+		apiClient, ctx, err := config.GetPreparedApiClient(config.DefaultConfig)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		/*GetBulkMetaParams contains all the parameters to send to the API endpoint
+		for the get bulk meta operation typically these are written to a http.Request
+		*/
+		params := &meta_service.GetBulkMetaParams{
+			Body: &models.RestGetBulkMetaRequest{NodePaths: []string{
+				//the workspaces from whom the files are listed
+				"/*",
+				"personal-files/*",
+			}},
+			Context:    ctx,
+			HTTPClient: httpClient,
+		}
+
+		//assigns the files data retrieved above in the results variable
+		result, err := apiClient.MetaService.GetBulkMeta(params)
+		if err != nil {
+			fmt.Printf("could not list meta: %s\n", err.Error())
+			log.Fatal(err)
+		}
+
+		//prints the path therefore the name of the files listed
+		if len(result.Payload.Nodes) > 0 {
+			fmt.Printf("* %d meta\n", len(result.Payload.Nodes))
+			for _, u := range result.Payload.Nodes {
+				fmt.Println("  -", u.Path)
+			}
+		}
+
+	},
+}
+
+func init() {
+	ExampleCmd.AddCommand(listFiles)
+}
---
+++ example/cmd/list_workspaces.go
@@ -0,0 +1,53 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/spf13/cobra"
+
+	"github.com/pydio/cells-sdk-go/client/workspace_service"
+	"github.com/pydio/cells-sdk-go/config"
+	"github.com/pydio/cells-sdk-go/models"
+)
+
+var listWorkspaces = &cobra.Command{
+	Use:   "list-workspaces",
+	Short: "lw",
+	Long:  `List all the workspaces`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		httpClient := config.GetHttpClient(config.DefaultConfig)
+		apiClient, ctx, err := config.GetPreparedApiClient(config.DefaultConfig)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		//retrieves the users using the searchWorkspacesParams function
+		params := &workspace_service.SearchWorkspacesParams{
+			Body:       &models.RestSearchWorkspaceRequest{CountOnly: true},
+			Context:    ctx,
+			HTTPClient: httpClient,
+		}
+
+		//assigns the workspaces data retrieved above in the results variable
+		result, err := apiClient.WorkspaceService.SearchWorkspaces(params)
+		if err != nil {
+			fmt.Printf("could not list workspaces: %s\n", err.Error())
+			log.Fatal(err)
+		}
+
+		//prints the workspace label
+		if len(result.Payload.Workspaces) > 0 {
+			fmt.Printf("* %d workspace found\n", len(result.Payload.Workspaces))
+			for _, u := range result.Payload.Workspaces {
+				fmt.Println("  - " + u.Label)
+			}
+		}
+
+	},
+}
+
+func init() {
+	ExampleCmd.AddCommand(listWorkspaces)
+}
