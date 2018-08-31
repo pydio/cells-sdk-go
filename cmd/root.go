8db4ca139d1d7acfa15f6cb68b7eb792e7b9bd3d
Bruno Sinou <bruno.sinou@posteo.de>
2018-08-31 08:56:48 +0200

    Some enhancements on the Root Command, also remove pointer to default config file

--- cmd/root.go
+++ cmd/root.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -24,10 +23,30 @@ var (
 	skipVerify bool
 )
 
+// RootCmd is the parent of all REST API Client commands.
+// It encapsulates the mechanism to configure the communication with a running Cells server.
 var RootCmd = &cobra.Command{
 	Use:   os.Args[0],
-	Short: "Rest Client of Pydio Cells",
-	Long:  `Pydio Cells client for managing API`,
+	Short: "Simple CLI Client for Pydio Cells REST API",
+	Long: `
+# Simple CLI Client for Pydio Cells REST API.
+	
+You should either provide config parameters using the flags or a path to a valid json configuration file.
+	
+### Syntax
+
+$ ` + os.Args[0] + ` [command] [flags] args...
+
+### Examples
+
+Use a json configuration file that lays in the same folder (see config/config.sample.json file for an example)  
+$ ` + os.Args[0] + ` [command] --c config.json
+
+Use parameters as flag 
+$ ` + os.Args[0] + ` [command] -protocol http --u localhost:8080 --i pydio-front --s pydio-secret --l admin --p pass123 
+
+
+	`,
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
 
 		// Parse from parameters
@@ -46,19 +65,33 @@ var RootCmd = &cobra.Command{
 
 		// Parse from config
 		if configFile == "" {
-			log.Fatal("Please provide a path to the configuration file")
+			cmd.Help()
+			cmd.Println("\n--\nError: please provide a path to the configuration file or provide config parameters (See above).")
+			os.Exit(1)
 		}
-		if data, e := ioutil.ReadFile(configFile); e == nil {
-			var c config.SdkConfig
-			if e := json.Unmarshal(data, &c); e != nil {
-				log.Fatal("Cannot decode config content", e)
-			}
-			config.DefaultConfig = &c
-			fmt.Println("Connecting to " + config.DefaultConfig.Url)
-			fmt.Println("")
-		} else {
-			log.Fatal("Cannot read file, root cause:", e)
+
+		data, e := ioutil.ReadFile(configFile)
+		if e != nil {
+			log.Fatal("cannot read config file:", e)
 		}
+
+		var c config.SdkConfig
+		if e = json.Unmarshal(data, &c); e != nil {
+			log.Fatal("Cannot decode config content for file at", configFile, "- route cause:", e)
+		}
+		config.DefaultConfig = &c
+
+		// if data, e := ioutil.ReadFile(configFile); e == nil {
+		// 	var c config.SdkConfig
+		// 	if e := json.Unmarshal(data, &c); e != nil {
+		// 		log.Fatal("Cannot decode config content", e)
+		// 	}
+		// 	config.DefaultConfig = &c
+		// 	// fmt.Println("Connecting to " + config.DefaultConfig.Url)
+		// 	// fmt.Println("")
+		// } else {
+		// 	log.Fatal("Cannot read file at", configFile, ", root cause:", e)
+		// }
 
 	},
 	Run: func(cmd *cobra.Command, args []string) {
@@ -68,7 +101,7 @@ var RootCmd = &cobra.Command{
 
 func init() {
 	flags := RootCmd.PersistentFlags()
-	flags.StringVarP(&configFile, "config", "c", "config.json", "Path to the configuration file")
+	flags.StringVarP(&configFile, "config", "c", "", "Relative path to the configuration file")
 
 	flags.StringVar(&protocol, "protocol", "http", "Http scheme to server")
 	flags.StringVarP(&host, "url", "u", "", "Http URL to server")
