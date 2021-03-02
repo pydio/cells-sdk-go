944628fc76f5666718a39161d55a38fbee6218d3
Bruno Sinou <bruno.sinou@posteo.de>
2021-03-02 10:55:30 +0100

    Fix typo in comment

--- transport/http/client.go
+++ transport/http/client.go
@@ -5,10 +5,10 @@ import (
 
 	"github.com/pydio/cells-sdk-go/transport"
 
-	"github.com/pydio/cells-sdk-go"
+	cells_sdk "github.com/pydio/cells-sdk-go"
 )
 
-// GetClient provides an option to rather use an http client that ignore SSL certificate issues.
+// GetClient provides an option to rather use an HTTP client that ignores SSL certificate issues.
 func GetClient(sdkConfig *cells_sdk.SdkConfig) *http.Client {
 	return &http.Client{Transport: transport.New(
 		transport.WithSkipVerify(sdkConfig.SkipVerify),
