2ef3a63e91c6fa26a95be51815364addd9e52be4
Bruno Sinou <bruno.sinou@posteo.de>
2022-09-09 12:47:29 +0200

    Adapt to Cells v4 API

---
+++ v4/transport/rest/client.go
@@ -0,0 +1,48 @@
+package rest
+
+import (
+	"context"
+	"net/url"
+
+	"github.com/go-openapi/runtime"
+	"github.com/go-openapi/runtime/client"
+	"github.com/go-openapi/strfmt"
+
+	cells_sdk "github.com/pydio/cells-sdk-go/v4"
+	client2 "github.com/pydio/cells-sdk-go/v4/client"
+	"github.com/pydio/cells-sdk-go/v4/transport"
+)
+
+func GetClient(sdkConfig *cells_sdk.SdkConfig, anon bool) (context.Context, *client2.PydioCellsRestAPI, error) {
+
+	c, t, e := GetClientTransport(sdkConfig, anon)
+	if e != nil {
+		return nil, nil, e
+	}
+	return c, client2.New(t, strfmt.Default), nil
+
+}
+
+func GetClientTransport(sdkConfig *cells_sdk.SdkConfig, anonymous bool) (context.Context, runtime.ClientTransport, error) {
+
+	u, e := url.Parse(sdkConfig.Url)
+	if e != nil {
+		return nil, nil, e
+	}
+	tp := client.New(u.Host, transport.CellsApiResourcePath, []string{u.Scheme})
+	transportOptions := []interface{}{
+		transport.WithSkipVerify(sdkConfig.SkipVerify),
+		transport.WithCustomHeaders(sdkConfig.CustomHeaders),
+	}
+	if !anonymous {
+		tp, e := transport.TokenProviderFromConfig(sdkConfig)
+		if e != nil {
+			return nil, nil, e
+		}
+		transportOptions = append(transportOptions, transport.WithBearer(tp))
+	}
+	tp.Context = context.Background()
+	tp.Transport = transport.New(transportOptions...)
+
+	return tp.Context, tp, nil
+}
