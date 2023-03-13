a8f1aaca6e27b3469d8e611579e0e56c57139f2c
Bruno Sinou <bruno.sinou@posteo.de>
2023-03-13 10:28:16 +0100

    Put everything back to the root - First pass, delete old code

--- v2/example/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/pydio/cells-sdk-go/v2/example/cmd"
-)
-
-func main() {
-	cmd.ExampleCmd.Execute()
-}
--- v3/example/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/pydio/cells-sdk-go/v3/example/cmd"
-)
-
-func main() {
-	cmd.ExampleCmd.Execute()
-}
