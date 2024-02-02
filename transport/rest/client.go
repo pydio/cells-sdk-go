60d0ee15c7df3719f91c58d13389957e4e5f3655
Bruno Sinou <bruno.sinou@posteo.de>
2024-02-02 16:41:56 +0100

    Fix glitches

--- transport/rest/client.go
+++ transport/rest/client.go
@@ -1,11 +1,9 @@
 package rest
 
 import (
-	"context"
 	"github.com/pydio/cells-sdk-go/v5/transport/http"
 	"net/url"
 
-	"github.com/go-openapi/runtime"
 	"github.com/go-openapi/runtime/client"
 	"github.com/go-openapi/strfmt"
 
@@ -43,36 +41,36 @@ func GetApiTransport(sdkConfig *cells_sdk.SdkConfig, anonymous bool) (*client.Ru
 	return tp, nil
 }
 
-func GetClient(sdkConfig *cells_sdk.SdkConfig, anon bool) (context.Context, *client2.PydioCellsRestAPI, error) {
+//func GetClient(sdkConfig *cells_sdk.SdkConfig, anon bool) (context.Context, *client2.PydioCellsRestAPI, error) {
+//
+//	c, t, e := GetClientTransport(sdkConfig, anon)
+//	if e != nil {
+//		return nil, nil, e
+//	}
+//	return c, client2.New(t, strfmt.Default), nil
+//
+//}
 
-	c, t, e := GetClientTransport(sdkConfig, anon)
-	if e != nil {
-		return nil, nil, e
-	}
-	return c, client2.New(t, strfmt.Default), nil
-
-}
-
-func GetClientTransport(sdkConfig *cells_sdk.SdkConfig, anonymous bool) (context.Context, runtime.ClientTransport, error) {
-
-	u, e := url.Parse(sdkConfig.Url)
-	if e != nil {
-		return nil, nil, e
-	}
-	tp := client.New(u.Host, transport.CellsApiResourcePath, []string{u.Scheme})
-	transportOptions := []interface{}{
-		http.WithSkipVerify(sdkConfig.SkipVerify),
-		http.WithCustomHeaders(sdkConfig.CustomHeaders),
-	}
-	if !anonymous {
-		tp, e := transport.TokenProviderFromConfig(sdkConfig)
-		if e != nil {
-			return nil, nil, e
-		}
-		transportOptions = append(transportOptions, http.WithBearer(tp))
-	}
-	tp.Context = context.Background()
-	tp.Transport = transport.New(transportOptions...)
-
-	return tp.Context, tp, nil
-}
+//func GetClientTransport(sdkConfig *cells_sdk.SdkConfig, anonymous bool) (context.Context, runtime.ClientTransport, error) {
+//
+//	u, e := url.Parse(sdkConfig.Url)
+//	if e != nil {
+//		return nil, nil, e
+//	}
+//	tp := client.New(u.Host, transport.CellsApiResourcePath, []string{u.Scheme})
+//	transportOptions := []interface{}{
+//		http.WithSkipVerify(sdkConfig.SkipVerify),
+//		http.WithCustomHeaders(sdkConfig.CustomHeaders),
+//	}
+//	if !anonymous {
+//		tp, e := transport.TokenProviderFromConfig(sdkConfig)
+//		if e != nil {
+//			return nil, nil, e
+//		}
+//		transportOptions = append(transportOptions, http.WithBearer(tp))
+//	}
+//	tp.Context = context.Background()
+//	tp.Transport = transport.New(transportOptions...)
+//
+//	return tp.Context, tp, nil
+//}
